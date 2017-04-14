24305ede3dcbbde37a277ce19a6a1804c4c84c4f
JasonBonvie <jasonbonvie@Jasons-MacBook.local>
2017-04-14 15:34:07 -0400

    Added the color to the text

--- RubixCube/Input.cpp
+++ RubixCube/Input.cpp
@@ -1,4 +1,25 @@
 #include "Input.hpp"
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+#define CLEAR "\033[2J"  // clear screen escape code
 
 map< char, vector<char> > InputUI::cubeMap;
 
@@ -44,28 +65,29 @@ void InputUI::printFace(vector<char> face) {
 
 void InputUI::printCube() {
     char color;
+
     vector<char> empty(9, ' ');
 
     for (int i=0; i<6; i++) {
         color = colorList[i];
         switch(i) {
           case 0:
-        cout << "Red (Yellow facing up) ------" << endl;
+        cout << BOLDRED<<"Red side"<<" (Yellow side facing perpendicular to your body) ------" << endl;
         break;
           case 1:
-        cout << "Green (Yellow facing up) ----" << endl;
+        cout << BOLDGREEN <<"Green side"<< "(Yellow side facing perpendicular to your body) ----" << endl;
         break;
           case 2:
-        cout << "Orange (Yellow facing up) ---" << endl;
+        cout << BOLDMAGENTA << "Orange side "<< "(Yellow side facing perpendicular to your body) ---" << endl;
         break;
           case 3:
-        cout << "Blue (Yellow facing up) -----" << endl;
+        cout << BOLDBLUE<<"Blue side" << "(Yellow side facing perpendicular to your body) -----" << endl;
         break;
           case 4:
-        cout << "Yellow (Orange facing up) --" << endl;
+        cout << BOLDYELLOW<<"Yellow side"<<" (Orange side facing perpendicular to your body) --" << endl;
         break;
           case 5:
-        cout << "White (Red facing up) ------" << endl;
+        cout << BOLDWHITE<<"White side "<<" (Red side facing perpendicular to your body) ------" << endl;
         break;
         }
         if (cubeMap[color].empty())
