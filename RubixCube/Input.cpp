6c78295dbaa4e0eb0b419b1470e4258f4793c7b6
warjr0000 <warjr0000@hotmail.com>
2017-04-14 16:03:08 -0400

    Colors
    Pretty

--- RubixCube/Input.cpp
+++ RubixCube/Input.cpp
@@ -1,21 +1,11 @@
 #include "Input.hpp"
 #define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
 
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDRED     "\033[7m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
 #define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDORANGE  "\033[1m\033[31m"
 
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
 #define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
 #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
@@ -72,22 +62,22 @@ void InputUI::printCube() {
         color = colorList[i];
         switch(i) {
           case 0:
-        cout << BOLDRED<<"Red side"<<" (Yellow side facing perpendicular to your body) ------" << endl;
+        cout << BOLDRED << "Red side" << " (Yellow side facing perpendicular to your body)" << RESET << endl;
         break;
           case 1:
-        cout << BOLDGREEN <<"Green side"<< "(Yellow side facing perpendicular to your body) ----" << endl;
+        cout << BOLDGREEN << "Green side" << "(Yellow side facing perpendicular to your body)" << RESET << endl;
         break;
           case 2:
-        cout << BOLDMAGENTA << "Orange side "<< "(Yellow side facing perpendicular to your body) ---" << endl;
+        cout << BOLDORANGE << "Orange side "<< "(Yellow side facing perpendicular to your body)" << RESET << endl;
         break;
           case 3:
-        cout << BOLDBLUE<<"Blue side" << "(Yellow side facing perpendicular to your body) -----" << endl;
+        cout << BOLDCYAN << "Blue side" << "(Yellow side facing perpendicular to your body)" << RESET << endl;
         break;
           case 4:
-        cout << BOLDYELLOW<<"Yellow side"<<" (Orange side facing perpendicular to your body) --" << endl;
+        cout << BOLDYELLOW << "Yellow side" << " (Orange side facing perpendicular to your body)" << RESET << endl;
         break;
           case 5:
-        cout << BOLDWHITE<<"White side "<<" (Red side facing perpendicular to your body) ------" << endl;
+        cout << BOLDWHITE <<"White side " << " (Red side facing perpendicular to your body)" << RESET << endl;
         break;
         }
         if (cubeMap[color].empty())
--- RubixCube/Output.cpp
+++ RubixCube/Output.cpp
@@ -1,6 +1,18 @@
 #include "Output.hpp"
 #include <iostream>
 
+#define RESET   "\033[0m"
+
+#define BOLDRED     "\033[7m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDORANGE  "\033[1m\033[31m"
+
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+#define CLEAR "\033[2J"  // clear screen escape code
+
 OutputNode::OutputNode(void) {
   next = nullptr;
   previous = nullptr;
@@ -8,12 +20,35 @@ OutputNode::OutputNode(void) {
 }
 
 void OutputNode::Print(void) {
+
+  string font;
+  switch (color.at(0)) {
+    case 'R':
+      font = BOLDRED;
+      break;
+    case 'G':
+      font = BOLDGREEN;
+      break;
+    case 'O':
+      font = BOLDORANGE;
+      break;
+    case 'B':
+      font = BOLDCYAN;
+      break;
+    case 'Y':
+      font = BOLDYELLOW;
+      break;
+    case 'W':
+      font = BOLDWHITE;
+      break;
+  }
+
   if (dir == 2 || dir == -2) {
-    cout << color << " 180 degrees" << endl;
+    cout << font << color << " 180 degrees" << RESET << endl;
   } else if (dir == 1) {
-    cout << color << " counter-clockwise" << endl;
+    cout << font << color << " counter-clockwise" << RESET << endl;
   } else if (dir == -1) {
-    cout << color << " clockwise" << endl;
+    cout << font << color << " clockwise" << RESET << endl;
   } else {
     cout << "ERROR: Broken output" << endl;
     exit(0);
